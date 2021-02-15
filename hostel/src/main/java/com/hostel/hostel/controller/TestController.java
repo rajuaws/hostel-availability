ae7c0182ebcff2bfd86405dbf20fa029d2809c85
Muduguri Nagaraju <mn037518@broadcom.net>
2021-02-15 23:16:23 +0530

    intial commit hostel app commiting

---
+++ hostel/src/main/java/com/hostel/hostel/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.hostel.hostel.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/hostel")
+	public String getHostelDetails() {
+		return "Same place hostel..";
+	}
+
+}
